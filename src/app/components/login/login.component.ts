0588ab98b4fc12c392f8b1ac9d698741a390c225
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-15 10:18:59 +0300

    filled login and register components

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
+
+  createLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    });
+  }
+
+  login() {
+
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe(response => {
+        this.toastrService.info("Başarıyla giriş yaptınız, admin sayfasına yönlendiriliyorsunuz..");
+        localStorage.setItem("token", response.data.token);
+        this.router.navigate(["/admin"]);
+      }, responseError => {
+        this.toastrService.error(responseError.error);
+      });
+    }
+
+    else {
+      this.toastrService.error("Formu düzgün doldurunuz");
+    }
+  }
+
+  redirectToRegister(): void {
+    this.router.navigate(["register"])
+  }
+
+  // Form Input Check
+
+  get email() { return this.loginForm.get('email') }
+  get password() { return this.loginForm.get('password') }
 
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private toastrService: ToastrService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  get firstName() { return this.registerForm.get('firstName') }
+  get lastName() { return this.registerForm.get('lastName') }
+  get email() { return this.registerForm.get('email') }
+  get password() { return this.registerForm.get('password') }
+  // get confirmPassword() { return this.registerForm.get('firstName') }
+
+  register(): void {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe(response => {
+        this.toastrService.info("Başarıyla kullanıcı oluşturuldu, giriş sayfasına yönlendiriliyorsunuz...");
+        this.router.navigate(["/login"])
+      }, responseError => {
+        this.toastrService.error(responseError.error);
+      });
+    }
   }
 
 }

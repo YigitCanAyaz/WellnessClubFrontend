d6d2a460f1a6ce16f995820586b7b85240257795
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-17 02:14:10 +0300

    filling user height component

--- src/app/components/admin/user-height/user-height-add/user-height-add.component.ts
+++ src/app/components/admin/user-height/user-height-add/user-height-add.component.ts
@@ -1,4 +1,10 @@
+import { UserForInfoDetail } from './../../../../models/details/userForInfoDetail';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserHeightService } from 'src/app/services/user-height.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-height-add',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserHeightAddComponent implements OnInit {
 
-  constructor() { }
+  userheightAddForm: FormGroup;
+  users: UserForInfoDetail[] = [];
+
+  constructor(private userHeightService: UserHeightService, private userService: UserService, private formBuilder: FormBuilder, private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createUserHeightAddForm();
+    this.getAllUsers();
+  }
+
+  createUserHeightAddForm(): void {
+    this.userheightAddForm = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      userId: ["", [Validators.required]],
+      year: ["", [Validators.required]]
+    });
+  }
+
+  addUserHeight(): void {
+    console.log(this.userheightAddForm.controls['userId'].value);
+    console.log(this.userheightAddForm.value);
+    if (this.userheightAddForm.valid) {
+      this.userHeightService.add(this.userheightAddForm.value).subscribe(response => {
+        this.toastrService.info(response.message, this.userheightAddForm.controls['name'].value);
+        this.router.navigate(["/admin/userheights/list"]);
+      }, responseError => {
+        this.toastrService.error(responseError.error);
+      });
+    }
+
+    else {
+      this.toastrService.error("Fill the form correctly");
+    }
+  }
+
+  getAllUsers(): void {
+    this.userService.getAll().subscribe(response => {
+      this.users = response.data;
+    })
   }
 
 }
